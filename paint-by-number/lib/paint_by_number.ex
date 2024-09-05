d3c102226950d72ec1a5b0af9f66c030a87c81df
0xJohnnyboy <lambert.theo@proton.me>
2024-09-05 11:39:45 +0200

    submit paint-by-number

--- paint-by-number/lib/paint_by_number.ex
+++ paint-by-number/lib/paint_by_number.ex
@@ -19,14 +19,18 @@ defmodule PaintByNumber do
   def get_first_pixel(<<>>, _color_count), do: nil
 
   def get_first_pixel(picture, color_count) do
-    
     size = palette_bit_size(color_count)
-    <<head::size(size), _>> = picture
-    head
+    <<first::size(size), _::bitstring>> = picture
+    first
   end
 
   def drop_first_pixel(<<>>, _color_count), do: <<>>
-  def drop_first_pixel(<<_, rest::bitstring>>, _color_count), do: rest
+
+  def drop_first_pixel(picture, color_count) do
+    size = palette_bit_size(color_count)
+    <<_::size(size), rest::bitstring>> = picture
+    rest
+  end
 
   def concat_pictures(picture1, picture2), do: <<picture1::bitstring, picture2::bitstring>>
 end
